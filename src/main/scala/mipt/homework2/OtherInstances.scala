98d415ae4416ecd8b75144bbdf5d735c4e671239
nkon23 <nikita.kon723@gmail.com>
2023-03-13 22:15:46 +0300

    done hw 1

--- src/main/scala/mipt/homework2/OtherInstances.scala
+++ src/main/scala/mipt/homework2/OtherInstances.scala
@@ -5,9 +5,12 @@ import cats.Functor
 trait OtherInstances:
   type Arr[-C, +A] = C => (List[A] => C) => A
 
-  given [C]: Functor[Arr[C, *]] =
-    task"Реализуйте инстанс Functor для Arr[C, *] (по второму аргументу)" (4, 0)
+  given [C]: Functor[Arr[C, *]] = new Functor[Arr[C, *]] {
+    override def map[A, B](fa: Arr[C, A])(
+        f: A => B
+    ): Arr[C, B] = (c: C) => (fbc: List[B] => C) => f(fa(c)(fbc.compose(_.map(f))))
+  }
 
-  given [F[_]: Functor, G[_]: Functor]: Functor[[x] =>> F[G[x]]] =
-    task"""Реализуйте инстанс для композиции функторов и
-           докажите законы функтора для этого инстанса""" (4, 1)
+  given [F[_]: Functor, G[_]: Functor]: Functor[[x] =>> F[G[x]]] = new Functor[[x] =>> F[G[x]]] {
+    override def map[A, B](fga: F[G[A]])(f: A => B): F[G[B]] = Functor[F].map(fga)(Functor[G].map(_)(f))
+  }
