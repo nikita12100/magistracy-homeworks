3b6dfce6c53ae6783f52128ad44a8bb99767b737
nkon23 <nikita.kon723@gmail.com>
2023-05-14 11:59:45 +0300

    done hw

--- src/main/scala/mipt/homework10/ContextOps.scala
+++ src/main/scala/mipt/homework10/ContextOps.scala
@@ -15,6 +15,6 @@ trait ContextOps[Context, F[_]]:
   def getContext(using Ask[F, Context]): F[Context] = Ask[F, Context].ask
 
   def getSubContext[A](using Ask[F, Context], Contains[Context, A]): F[A] =
-    task"Реализуйте метод, возвращающий значение, содержащееся в контексте"(2, 1)
+    Ask[F, Context].reader(context => Contains[Context, A].extract(context))
   def getEmbedSubContext[U[_[_]]](using Ask[F, Context], Contains[Context, U[F]], Embed[U], FlatMap[F]): U[F] =
-    task"Реализуйте метод, возвращающий Embed алгебру, содержащуюся в контексте"(2, 2)
+    Embed[U].embed[F](getSubContext)
--- src/main/scala/mipt/homework10/UserLookup.scala
+++ src/main/scala/mipt/homework10/UserLookup.scala
@@ -1,5 +1,7 @@
 package mipt.homework10
 
+import cats.Applicative
+import cats.syntax.traverse.*
 import glass.{Contains, Items, Property}
 import mipt.utils.Homeworks.TaskSyntax
 
@@ -10,6 +12,16 @@ case class UserId(id: Int)
 case class User(id: UserId, role: Role, friends: Vector[UserId])
 
 object UserLookup:
-  val idLens: Contains[User, Int] = task"Реализуйте оптику, фокусирующуюся на id внутри User"(1, 1)
-  val adminOptional: Property[User, Role.Admin.type] = task"Реализуйте оптику, фокусирующуюся на роли Admin у User"(1, 2)
-  val friendsTraversal: Items[User, UserId] = task"Реализуйте оптику, фокусирующуюся на друзьях у User"(1, 3)
+  val idLens: Contains[User, Int] = new Contains[User, Int] :
+    override def set(s: User, b: Int): User = s.copy(id = UserId(b))
+    override def extract(s: User): Int = s.id.id
+
+  val adminOptional: Property[User, Role.Admin.type] = new Property[User, Role.Admin.type]:
+    override def set(s: User, b: Role.Admin.type): User = s.copy(role = b)
+    override def narrow(s: User): Either[User, Role.Admin.type] = s.role match
+      case admin: Role.Admin.type => Right(admin)
+      case _                      => Left(s)
+
+  val friendsTraversal: Items[User, UserId] = new Items[User, UserId]:
+    override def traverse[F[+_] : Applicative](s: User)(f: UserId => F[UserId]): F[User] =
+      Applicative[F].map(s.friends.traverse(f))(friendsV => s.copy(friends = friendsV))
